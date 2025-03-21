d9bbcae63f6db5b787ee61f7c536906f806f3688
Liborio <amaroico2013@hotmail.com>
2025-03-21 13:11:01 -0600

    Sexta tanda de programas en Spring

---
+++ demoAPI-Gateway-1/src/main/java/com/mx/demoAPIGateway/DemoApiGateway1Application.java
@@ -0,0 +1,13 @@
+package com.mx.demoAPIGateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApiGateway1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApiGateway1Application.class, args);
+	}
+
+}
---
+++ demoAPI-Gateway-1/src/test/java/com/mx/demoAPIGateway/DemoApiGateway1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mx.demoAPIGateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApiGateway1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
